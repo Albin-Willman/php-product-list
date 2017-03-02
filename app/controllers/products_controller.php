400a5bb5a97ded28476f1a6672611d77a75fc1e9
Albin <j.albin.willman@gmail.com>
2017-03-02 23:12:31 +0100

    Load actual data

--- app/controllers/products_controller.php
+++ app/controllers/products_controller.php
@@ -1,11 +1,11 @@
 <?php
 require_once('../app/utils/xml_product_parser.php');
 require_once('../app/views/products/index.php');
-define('DATA_PATH', '../data/test_data.xml');
+define('DATA_PATH', '../data/products.xml');
 
 class ProductsController {
   public function index() {
-    $parser = new XMLProductParser('../data/test_data.xml');
+    $parser = new XMLProductParser(DATA_PATH);
     $products = $parser->product_list();
 
     // TODO: Sort products;
